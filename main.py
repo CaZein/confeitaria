763ac694854a766a43ce7046f153bf444e8474c8
CaZein <127060674+CaZein@users.noreply.github.com>
2024-07-26 15:14:20 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -3,9 +3,14 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-@app.route("/")
+@app.route("/home")
 def inicial():
     return render_template("index.html")
+
+@app.route("/contatos")
+def contato():
+   return render_template("contato.html")
+
 
 
 if __name__ == '__main__':
